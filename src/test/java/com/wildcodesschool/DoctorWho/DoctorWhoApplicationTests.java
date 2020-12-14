5ffdc4ab98cd4dd88800d9444d9dca4be5a91e32
AICHOUN-C <cherif.aichoun@gmail.com>
2020-12-14 14:48:22 +0100

    init: project initialization

---
+++ src/test/java/com/wildcodesschool/DoctorWho/DoctorWhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodesschool.DoctorWho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorWhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
